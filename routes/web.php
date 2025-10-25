be5debc78253d7f20d99d400bd7ce0339ff11aa0
Galhoom <102315329+Galhoom22@users.noreply.github.com>
2025-10-25 08:35:09 +0300

    feat(view): create about page view extending app layout

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/shop', function () {
+    return 'Shop Page Placeholder'; // Temporary placeholder
+})->name('shop.index');
+
+Route::get('/contact', function () {
+    return 'Contact Page Placeholder'; // Temporary placeholder
+})->name('contact');
